d030156198142abb9a8f4b1db1989c3372f13b89
Alexander Knips <alex.knips@kariusdx.com>
2024-01-15 19:44:56 -0800

    skills

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import Particles from "./components/particles";
 const navigation = [
   { name: "Projects", href: "/projects" },
   { name: "Contact", href: "/contact" },
+  { name: "Skills", href: "/skills" },
+
 ];
 
 export default function Home() {
